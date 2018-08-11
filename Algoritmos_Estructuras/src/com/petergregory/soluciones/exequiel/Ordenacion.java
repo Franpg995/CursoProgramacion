d568eb9f0465b6c73c5587e1c222c315e181fd9e
Exequiel Zeballos <exequiel222@gmail.com>
2018-08-11 19:28:58 -0300

    Agrego consigna de soluciones

--- Algoritmos_Estructuras/src/com/petergregory/soluciones/exequiel/Ordenacion.java
+++ Algoritmos_Estructuras/src/com/petergregory/soluciones/exequiel/Ordenacion.java
@@ -3,13 +3,22 @@ package com.petergregory.soluciones.exequiel;
 import com.petergregory.soluciones.IOrdenacion;
 
 public class Ordenacion implements IOrdenacion {
+
     @Override
     public int[] ordenarMayorAMenor(int[] entradaAOrdenar) {
+
         return new int[0];
     }
 
     @Override
     public int[] ordenarMenorAMayor(int[] entradaAOrdenar) {
+        /*
+         *Revisar las cartas
+         *Me doy si estan o no ordenadas
+         * Si la de la izq es menor que la de la derecha está ok, paso a la otra
+         * Si la de la izq es mayor que la de la derecha está desordenado, y las cambio
+         * Si la de la izq es igual que la de la derecha está ok, paso a la otra
+         */
         return new int[0];
     }
 }
--- Algoritmos_Estructuras/src/com/petergregory/soluciones/gregory/Ordenacion.java
+++ Algoritmos_Estructuras/src/com/petergregory/soluciones/gregory/Ordenacion.java
@@ -10,6 +10,14 @@ public class Ordenacion implements IOrdenacion {
 
     @Override
     public int[] ordenarMenorAMayor(int[] entradaAOrdenar) {
+        /*
+         *Revisar las cartas
+         *Me doy si estan o no ordenadas
+         * Si la de la izq es menor que la de la derecha está ok, paso a la otra
+         * Si la de la izq es mayor que la de la derecha está desordenado, y las cambio
+         * Si la de la izq es igual que la de la derecha está ok, paso a la otra
+         */
+
         return new int[0];
     }
 }
